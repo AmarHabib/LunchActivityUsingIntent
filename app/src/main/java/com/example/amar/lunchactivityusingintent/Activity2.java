1d2d0ed624f91af660cb61235204f6322b825d49
amarhabib <xcoder44@gmail.com>
2017-12-23 07:47:01 -0800

    Initial commit

---
+++ app/src/main/java/com/example/amar/lunchactivityusingintent/Activity2.java
@@ -0,0 +1,14 @@
+package com.example.amar.lunchactivityusingintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+    }
+}
